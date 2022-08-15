86474c2356f6405051dd78762e900d0251ee07e4
Juan David Garcia <juand.garcia.ajc@gmail.com>
2022-08-15 12:13:15 -0500

    core del negocio - Cambios de url

--- FrontEndAngular/PGrado_SICO_FrontEnd/src/app/producto-crud/producto.service.ts
+++ FrontEndAngular/PGrado_SICO_FrontEnd/src/app/producto-crud/producto.service.ts
@@ -9,33 +9,39 @@ import { HttpClient } from '@angular/common/http';
 export class ProductoService {
 
   //Url contiene todos los productos de la base de datos.
-  private baseURL = "http://localhost:8080/api/v1/productos";
+  private baseURL = "http://localhost:8080/api/v1/productos/";
 
   constructor(private httpClient: HttpClient) { }
 
   //Este metodo, nos sirve para obtener los productos (un observable es un patr0n de diseño)
   obtenerListaProductos(): Observable<Producto[]> {
-    return this.httpClient.get<Producto[]>(`${this.baseURL}`);
+    return this.httpClient.get<Producto[]>(this.baseURL + 'lista');
+  }
+
+ //este metodo sirve para obtener o buscar un producto
+  obtenerProductoPorId(id:number):Observable<Producto>{
+    return this.httpClient.get<Producto>(`${this.baseURL}detail/${id}`);
+  }
+
+
+ //este metodo sirve para obtener o buscar un producto
+  obtenerProductoPorDescripcion(descripcion:string):Observable<Producto>{
+    return this.httpClient.get<Producto>(`${this.baseURL}detaildescripcion/${descripcion}`);
   }
 
   //Este metodo, nos sirve para registrar un producto
   registrarProducto(producto: Producto): Observable<object> {
-    return this.httpClient.post(`${this.baseURL}`, producto);
+    return this.httpClient.post(this.baseURL + 'create', producto);
   }
 
-//Este metodo, nos sirve para registrar un producto
-actualizarProducto(id:number,producto: Producto): Observable<object> {
-  return this.httpClient.put(`${this.baseURL}/${id}`, producto);
-}
+    //Este metodo, nos sirve para registrar un producto
+  actualizarProducto(id:number,producto: Producto): Observable<object> {
+    return this.httpClient.put(`${this.baseURL}update/${id}`, producto);
+  }
 
- //este metodo sirve para obtener o buscar un producto
- obtenerProductoPorId(id:number):Observable<Producto>{
-  return this.httpClient.get<Producto>(`${this.baseURL}/${id}`);
-}
-
-//Este metodo, nos sirve para eliminar un producto
-eliminarProducto(id:number): Observable<object> {
-  return this.httpClient.delete(`${this.baseURL}/${id}`);
-}
+  //Este metodo, nos sirve para eliminar un producto
+  eliminarProducto(id:number): Observable<object> {
+    return this.httpClient.delete(`${this.baseURL}delete/${id}`);
+  }
 
 }
